7807401b6832f20c01b27994d3f7d37fb6487988
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2021-11-26 19:14:55 +0100

    feat: Ejemplo BBDD
    Crear un ejemplo de una base de datos en forma de diccionario en un archivo python

---
+++ src/BBDD_simulada.py
@@ -0,0 +1,13 @@
+#Mock
+BBDD_simulada = { 
+"model" : "Clio Sport",
+"brand" : "renault",
+"category": "premium",
+"type": "diesel",
+"color": "yellow",
+"asientos": 5,
+"year": 2001,
+"price": 20,
+"avaialble": True,
+"characteristics": ["tinted crystals", "turbo", "sportive", "compact"]
+}
