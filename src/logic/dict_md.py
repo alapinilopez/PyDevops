f663ffb4f21ecfb2efa9e0b6b3d57dc4494e297b
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2021-12-01 13:40:13 +0100

    refactor: Cambia nombres de variables
    Cambiar nombres de las variables ship a car

--- src/logic/dict_md.py
+++ src/logic/dict_md.py
@@ -1,25 +1,25 @@
 import sys
 sys.path.append(".") #Para poder debuguear sin tener que quitar el src.logic.
-from src.logic.BBDD_simulada import BBDD_simulada
+from src.BBDD_access.BBDD_query import BBDD_query
 
-
-def dict_md(BBDD_simulada):
+def dict_md(BBDD_query):
     try:
-        assert len(BBDD_simulada) > 0
+        assert len(BBDD_query) > 0
     except:
-        return "Su base de datos está vacía"
+        print("Su base de datos está vacía")
 
     # cada clave hacerla bold y su valor ponerlo al lado en texto plano
     bold = "**"
     key_value = ""
-    for ship_id, ship_info in BBDD_simulada.items():  # iterar el diccionario anidado
-        for key in ship_info:
-            key_value += bold + key + bold + ': ' + str(ship_info[key]) + "\n"
+    for car_id, car_info in BBDD_query.items():  # iterar el diccionario anidado
+        for key in car_info:
+            key_value += bold + key + bold + ': ' + str(car_info[key]) + "\n"
         key_value += "\n"
         # Añadimos los items al archivo md con la funcion open()
+        print(key_value)
         f = open("prueba.md", "w")
         f.write(key_value)
         f.close()
 
 
-dict_md(BBDD_simulada)
+dict_md(BBDD_query)
