d0d853aba61f5f001e0da6cc2601dfb7cd1cd82e
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2021-12-01 16:20:35 +0100

    feat(BBDD): Solicita la coleccion
    Acceder a los documentos que tenemos dentro de la BBDD

--- src/BBDD_access/BBDD_query.py
+++ src/BBDD_access/BBDD_query.py
@@ -2,13 +2,11 @@ import sys
 sys.path.append(".")
 from src.BBDD_access.BBDD_connect import *
 
-#def BBDD_query(collection):
-array = []
-dict = {}
-for document in collection.find({}):
-    array.append(document)
-print(array)
+def BBDD_query(collection):
+    array = []
+    for document in collection.find({}):
+        array.append(document)
+    return array
 #    f = open("BBDD.py", "w")
 #    f.write(document)
 #    f.close()
-#BBDD_query(collection)
