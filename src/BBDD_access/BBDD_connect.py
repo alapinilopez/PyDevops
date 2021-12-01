d75b162897f128448fbe3e1d58284f2aa1d70bfc
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2021-12-01 13:43:35 +0100

    feat(BBDD): Llama a la BBDD y devuelve los documentos
    Acceder a la BBDD y imprimir por pantalla los docs que tiene dentro

--- src/BBDD_access/BBDD_connect.py
+++ src/BBDD_access/BBDD_connect.py
@@ -1,8 +1,7 @@
 import pymongo
 
+uri = 'mongodb+srv://admin:admin@cluster0.thpbm.mongodb.net/PyDevops?retryWrites=true&w=majority'
+client = pymongo.MongoClient(uri) #Utilizamos una funcion de pymongo para conectarnos a la base de datos
 
-uri = "mongodb+srv://admin:admin@cluster0.thpbm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
-client = pymongo.MongoClient(uri)
-
-db = client.PyDevops
+db = client.PyDevops 
 collection = db.cars
--- src/logic/BBDD_simulada.py
+++ src/logic/BBDD_simulada.py
@@ -1,5 +1,5 @@
 # Mock
-BBDD_simulada = {1: {
+""" BBDD_simulada = {1: {
     "model": "Clio Sport",
     "brand": "renault",
     "category": "premium",
@@ -24,4 +24,17 @@ BBDD_simulada = {1: {
     "avaialble": True,
     "characteristics": ["tinted crystals", "turbo", "sportive", "compact"]
 }
-}
+} """
+{'_id': ObjectId('61a4ec1e1b8cc74d757149a8'), 'model': 'Clio Sport', 'brand': 'renault', 'category': 'premium', 'type': 'diesel', 'color': 'yellow', 'asientos': 5, 'year': 2001, 'price': 20, 'avaialble': True, 'characteristics': ['tinted crystals', 'turbo', 'sportive', 'compact']}
+{'_id': ObjectId('61a4ec1e1b8cc74d757149a9'), 'model': 'Renault 4', 'brand': 'renault', 'category': 'classic', 'type': 'gasoline', 'color': 'red', 'asientos': 5, 'year': 1992, 'price': 30, 'avaialble': True, 'characteristics': ['compact', 'low consumption', 'radio cassette']}
+{'_id': ObjectId('61a4ec1e1b8cc74d757149aa'), 'model': 'Kangoo', 'brand': 'renault', 'category': 'van', 'type': 'diesel', 'color': 'yellow', 'asientos': 5, 'year': 2008, 'price': 15, 'avaialble': True, 'characteristics': ['large', 'big trunk', 'tall', 'familiar']}
+{'_id': ObjectId('61a4ec1e1b8cc74d757149ab'), 'model': 'Seat Ibiza', 'brand': 'seat', 'category': 'common', 'type': 'diesel', 'color': 'white', 'asientos': 5, 'year': 2015, 'price': 13, 'avaialble': True, 'characteristics': ['low consumption', 'familliar']}
+{'_id': ObjectId('61a4ec1e1b8cc74d757149ac'), 'model': 'Seat Leaon FR', 'brand': 'seat', 'category': 'premium', 'type': 'hybrid', 'color': 'black', 'asientos': 5, 'year': 2017, 'price': 18, 'avaialble': True, 'characteristics': ['sportive', 'low consumption', 'heated seats', 'navigation assistant']}
+{'_id': ObjectId('61a4ec1e1b8cc74d757149ad'), 'model': 'Cordoba', 'brand': 'seat', 'category': 'common', 'type': 'electric', 'color': 'blue', 'asientos': 5, 'year': 2019, 'price': 16, 'avaialble': True, 'characteristics': ['low consumption', 'sunroof', 'familiar']}
+{'_id': ObjectId('61a4ec1e1b8cc74d757149ae'), 'model': 'Bettle', 'brand': 'volkswagen', 'category': 'classic', 'type': 'gasoline', 'color': 'blue', 'asientos': 2, 'year': 1990, 'price': 30, 'avaialble': True, 'characteristics': ['convertible', 'radio cassete', 'compact']}
+{'_id': ObjectId('61a4ec1e1b8cc74d757149af'), 'model': 'Golf', 'brand': 'volkswagen', 'category': 'common', 'type': 'diesel', 'color': 'white', 'asientos': 5, 'year': 2018, 'price': 16, 'avaialble': True, 'characteristics': ['compact', 'navigation assistant', 'sunroof']}
+{'_id': ObjectId('61a4ec1e1b8cc74d757149b0'), 'model': 'Sirocco', 'brand': 'volkswagen', 'category': 'premium', 'type': 'hybrid', 'color': 'white', 'asientos': 4, 'year': 2021, 'price': 27, 'avaialble': True, 'characteristics': ['sportive', 'tinted crystals', 'heated seats', 'navigations assistant', 'heated seats', 'sunroof']}
+{'_id': ObjectId('61a4ec1e1b8cc74d757149b1'), 'model': 'California', 'brand': 'vollkswagen', 'category': 'van', 'type': 'diesel', 'color': 'white', 'asientos': 6, 'year': 2016, 'price': 35, 'avaialble': True, 'characteristics': ['tall', 'large', 'navigation assistant', 'familiar', 'accomodation']}
+{'_id': ObjectId('61a4ec1e1b8cc74d757149b2'), 'model': 'Focus', 'brand': 'ford', 'category': 'common', 'type': 'hybrid', 'color': 'black', 'asientos': 5, 'year': 2017, 'price': 14, 'avaialble': True, 'characteristics': ['low consumption', 'familiar', 'tinted crystals']}
+{'_id': ObjectId('61a4ec1e1b8cc74d757149b3'), 'model': 'C-Max', 'brand': 'ford', 'category': 'common', 'type': 'diesel', 'color': 'grey', 'asientos': 5, 'year': 2015, 'price': 14, 'avaialble': True, 'characteristics': ['familiar', 'big trunk', 'large', 'sunroof']}
+{'_id': ObjectId('61a4ec1e1b8cc74d757149b4'), 'model': 'Galaxy', 'brand': 'ford', 'category': 'van', 'type': 'gasoline', 'color': 'black', 'asientos': 7, 'year': 2012, 'price': 18, 'avaialble': True, 'characteristics': ['familiar', 'sunroof', 'big trink', 'large']}
