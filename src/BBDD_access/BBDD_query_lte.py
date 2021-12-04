79363b870b05004385a8cb9153abe9e141042706
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2021-12-04 02:08:09 +0100

    perf(BBDD): Realiza una solicitud de la BBDD solo para los coches más baratos
    Sacar de la BBDD los documentos que tienen el precio menor o igual que 25

--- src/BBDD_access/BBDD_query_lte.py
+++ src/BBDD_access/BBDD_query_lte.py
@@ -1,1 +1,13 @@
 # hacer un find a la bbdd de todos los coches con valor menor a 25. lte
+import sys
+sys.path.append(".")
+from src.BBDD_access.BBDD_connect import *
+
+def BBDD_query_lte(collection):
+    cars_cheaper = []
+    for document in collection.find({"price": {"$lte": 25}}):
+        cars_cheaper.append(document)
+    return cars_cheaper
+
+if __name__ == "__main__":
+    assert BBDD_query_lte(collection) != -1
