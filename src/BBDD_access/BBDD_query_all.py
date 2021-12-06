32175424c099db8107b1a4070cba19eb0ab197b2
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2021-12-06 19:03:49 +0100

    perf(BBDD): Comprueba que se conecta a la BBDD
    Comprobar que se ha conectado correctamente a la BBDD

--- src/BBDD_access/BBDD_query_all.py
+++ src/BBDD_access/BBDD_query_all.py
@@ -3,8 +3,8 @@ sys.path.append(".")
 from src.BBDD_access.BBDD_connect import *
 
 def BBDD_query_all(collection):
-    array = []
+    cars_all = []
+    #buscamos todos los documentos de la coleccion 'cars_all' y lo añadimos a un array
     for document in collection.find({}):
-        array.append(document)
-    return array
-
+        cars_all.append(document)
+    return cars_all
--- src/BBDD_access/BBDD_query_year.py
+++ src/BBDD_access/BBDD_query_year.py
@@ -3,10 +3,10 @@ sys.path.append(".")
 from src.BBDD_access.BBDD_connect import *
 
 def BBDD_query_year(collection):
-    cars_year = []
-    for document in collection.find({"year": {"$gte": 2020}}):
-        cars_year.append(document)
-    return cars_year
+    cars_classics = []
+    for document in collection.find({"year": {"$lte": 2017}}):
+        cars_classics.append(document)
+    return cars_classics
 
 if __name__ == "__main__":
-    assert BBDD_query_year(collection) != -1
+    assert BBDD_query_year(collection) != -1 #Comprobamos que hay una colección
