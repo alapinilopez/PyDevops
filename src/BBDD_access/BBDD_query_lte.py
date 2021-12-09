b3c8da7b3c354aeefaab3591f88e176086534101
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2021-12-09 23:01:00 +0100

    refactor(BBDD): Imprime por pantalla si la BBDD esta vacia
    Añadir una condicion de que si la BBDD esta vacia, te lo diga

--- src/BBDD_access/BBDD_query_lte.py
+++ src/BBDD_access/BBDD_query_lte.py
@@ -1,10 +1,12 @@
-# hacer un find a la bbdd de todos los coches con valor menor a 25. lte
 import sys
 sys.path.append(".")
 from src.BBDD_access.BBDD_connect import *
 
+# hacer un find a la bbdd de todos los coches con valor menor a 25. lte
 def BBDD_query_lte(collection):
     cars_cheaper = []
     for document in collection.find({"price": {"$lte": 25}}):
         cars_cheaper.append(document)
+    if len(cars_cheaper) == 0:
+        print("Su base de datos no tiene coches con un precio inferior o igual a 25")
     return cars_cheaper
