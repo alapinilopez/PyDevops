df5ae9461fdd06a8c8b2b3c71e30316342032ca6
Andreu Sorell Pou <asorellpou@cifpfbmoll.eu>
2021-12-09 22:54:59 +0100

    feat(Conversion md): Crea la función principal
    Crear la función que llama a las otras

---
+++ src/logic/main.py
@@ -0,0 +1,10 @@
+from write_cars_all import *
+from write_cars_lte import *
+from write_cars_year import *
+from hugo_run import *
+
+
+write_cars_all(md_cars)
+write_cars_lte(md_cars_cheaper)
+write_cars_year(md_cars_classics)
+hugo_run()
